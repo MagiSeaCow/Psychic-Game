8ba1cd4d57318e683e14f4409fea7b978492e65d
MagiSeaCow <33107618+MagiSeaCow@users.noreply.github.com>
2018-10-13 19:58:32 -0400

    98% working version of the code, some quirks with the counters and resets of arrays, but it plays

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,27 +1,66 @@
+//Set all variables and intial counters.
+
 var arr = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
-var wins = 0;
+var playerWins = 0;
 
-var loses = 0;
+var compWins = 0;
 
-var guessLeft = 8;
+var guessLeft = 10;
 
-var userGuess = [];
+var userGuessLog = [];
 
-while (guessLeft < 8)
+var compRanGuess = "";
+
+//Computer picks a letter from the array
+//26 letter in the alphabet, randomly generated number will determine the letter the computer picks
+function compLetter() 
 {
-    var userInput = prompt("Guess my letter =) (lower case only)");
-    userGuess.push(userInput);
-    var compGuess = arr[Math.floor(Math.random() * arr.length)];
-    
-    if (userInput === compGuess)
-    {
-      wins++;
-      console.log(wins);
-    }
-    else
-    {
-      loses++;
-      console.log(loses);
-    }
+  compRanGuess = arr[(Math.floor(Math.random() * arr.length))];
+  console.log(compRanGuess);
+}
+//Store players input
+//.onkeypress captures and stores input in variable userGuess
+document.onkeypress = function (event) 
+{
+  var userGuess = event.key;
+
+      // Correct Guess Condition: if userGuess is equal to comRanGuess, player has won and will gain 1 point. Number of guessLeft reset back to 10. userGuessLog array is reset to empty
+      if (userGuess === compRanGuess) 
+      {
+        playerWins++;
+        guessLeft = 10;
+        userGuessLog = [];
+      }
+      compLetter();
+
+      // Lose Condition: if the number of guessLeft is equal to 0, the player has lost to the computer and computer will gain 1 point. Number of guessLeft reset back to 10. userGuessLog array is reset to empty
+      if (guessLeft === 0)
+      {
+        compWins++;
+        guessLeft = 10;
+        userGuessLog = [];
+      }
+
+      // Incorrect Guess Condition: if userGuess is not equal to comRanGuess, number of guessLeft is decreased by 1.
+      if (userGuess !== compRanGuess)
+      {
+        guessLeft--;
+      }
+
+      // "Logging" incorrect guesses to the webpage.
+
+      if (userGuessLog.indexOf(userGuess) < 0)
+      {
+        userGuessLog.push(userGuess);
+        document.getElementById('userGuessLog').innerHTML = userGuessLog;
+        console.log(userGuessLog);
+      };
+
+      //Passing values to front end HTML
+
+      document.getElementById('playerWins').innerHTML = playerWins;
+      document.getElementById('compWins').innerHTML = compWins;
+      document.getElementById('guessLeft').innerHTML = guessLeft;
+
 }
